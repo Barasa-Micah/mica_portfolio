94da9137f35c8357d424c40d21481ffaf6e12c79
username <barasamica@gmai.com>
2024-04-19 02:38:32 +0300

    Styled the Navigation bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/M-logo.jpg'
-import {link} from 'react-scroll';
-
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/M-logo.jpg';
+import { Link } from 'react-scroll';
+import ContactImg from '../../assets/contact.png'
 const Navbar = () => {
   return (
     <nav className='navbar'>
-        <img  src={logo} alt='logo' className='logo' width='130px'/>
+        <img  src={logo} alt='logo' className='logo'/>
         <div className='desktopMenu'>
-            <link className='desktopMenuListItem'></link>
-            <link className='desktopMenuListItem'></link>
-            <link className='desktopMenuListItem'></link>
-            <link className='desktopMenuListItem'></link>
+            <Link to="home" smooth={true} duration={500} className='desktopMenuListItem'>Home</Link>
+            <Link to="about" smooth={true} duration={500} className='desktopMenuListItem'>About</Link>
+            <Link to="portfolio" smooth={true} duration={500} className='desktopMenuListItem'>Portfolio</Link>
+            <Link to="clients" smooth={true} duration={500} className='desktopMenuListItem'>Clients</Link>
         </div>
         <button className='desktopMenuBtn'>
-            <img src='' alt='' className='desktopMenuImg'/>Contact Me</button>
+            <img src={ContactImg} alt='' className='desktopMenuImg'/>Contact Me
+        </button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
