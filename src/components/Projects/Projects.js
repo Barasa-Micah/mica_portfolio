2014d710b8dc2406b742bf9370a41511f4850080
username <barasamica@gmai.com>
2024-04-19 21:36:22 +0300

    Styled the portfoliio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,8 +5,8 @@ import './Projects.css'
 const Projects = () => {
   return (
     <section id='works'>
-        <h2 className='workstitle'>My Portfolio</h2>
-        <span className='worksDesc'>In my portfolio, each project embodies my sense of ownership and commitment to excellence. From inception to deployment, <br/> I've taken full responsibility for their success, ensuring every aspect aligns with the project's goals and objectives. <br/> This hands-on approach reflects not only my technical skills but also my dedication to delivering exceptional results</span>
+        <h2 className='worksTitle'>My Portfolio</h2>
+        <span className='worksDesc'>In my portfolio, each project embodies my sense of ownership and commitment to excellence. <br />From inception to deployment, I've taken full responsibility for their success, ensuring every aspect aligns with the <br/> project's goals and objectives.This hands-on approach reflects not only my technical skills <br/> but also my dedication to delivering exceptional results</span>
         <div className='worksImgs'>
             <img src='' alt='' className='worksImg'/>
             <img src='' alt='' className='worksImg'/>
