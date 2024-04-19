3f9000bef9670bb087bbe0926e51ca46584a9c7e
username <barasamica@gmai.com>
2024-04-19 16:11:48 +0300

    Tailored a diffeerent photo

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Intro.css';
-import bg from '../../assets/profile1.jpg';
+import bg from '../../assets/profile2.jpg';
 import { Link } from 'react-scroll';
 const Intro = () => {
   return (
-    <section className='introSection'> 
-        <div className='introContent'>
-            <span className='hello'>Hello,</span>
-            <span className='introtext'>I'm <span className='introName'>Barasa</span> <br /> A Software Developer</span>
-            <p className='introPara'>Explore a world where code creates creativity and dreams are just a line of code away, with me.</p>
-            <Link><button className='btn'>Hire Me</button></Link>
-        </div>
-        <img src={bg} alt='profile' className='bg' />
-    </section>
+    <section id='intro'> 
+    <div className='introContent'>
+        <span className='hello'>Hello,</span>
+        <span className='introtext'>I'm <span className='introName'>Barasa</span> <br /> A Software Developer</span>
+        <p className='introPara'>Explore a world where code creates creativity and <br/> dreams are just a line of code away, with me.</p>
+        <Link><button className='btn'>Hire Me</button></Link>
+    </div>
+    <img src={bg} alt='profile' className='bg' />
+</section>
+
   );
 }
 
