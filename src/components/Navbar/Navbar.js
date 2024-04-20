baf252fd3663533c3eb65b5e1e12b98a11ea9dfc
username <barasamica@gmai.com>
2024-04-20 12:36:35 +0300

    Made all navigations

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,9 +8,9 @@ const Navbar = () => {
     <nav className='navbar'>
         <img  src={logo} alt='logo' className='logo'/>
         <div className='desktopMenu'>
-            <Link activeClass='active' to="Home" spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
-            <Link activeClass='active'  to="about" spy={true} smooth={true} offset={-100} duration={500}  className='desktopMenuListItem'>About</Link>
-            <Link activeClass='active'  to="portfolio"spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Portfolio</Link>
+            <Link activeClass='active' to="intro" spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
+            <Link activeClass='active'  to="skills" spy={true} smooth={true} offset={-100} duration={500}  className='desktopMenuListItem'>Skills</Link>
+            <Link activeClass='active'  to="works"spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Portfolio</Link>
             <Link activeClass='active'  to="clients" spy={true} smooth={true} offset={-100} duration={500}  className='desktopMenuListItem'>Clients</Link>
         </div>
         <button className='desktopMenuBtn' onClick={() =>{
