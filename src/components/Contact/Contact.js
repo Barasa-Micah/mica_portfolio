1dd5670811162b571bd5d5f856480a8ffd3fabe4
username <barasamica@gmai.com>
2024-04-22 22:32:25 +0300

    Made social media links navigatable

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Contact.css";
-import LinkedIn from '../../assets/Linkedin.png';
-import X from '../../assets/Twitter.png';
-import Insta from '../../assets/Insta.png';
-import Github from '../../assets/git.png'
+import LinkedIn from "../../assets/Linkedin.png";
+import X from "../../assets/Twitter.png";
+import Insta from "../../assets/Insta.png";
+import Github from "../../assets/git.png";
+
 const Contact = () => {
   return (
     <section id="contactPage">
@@ -23,22 +24,38 @@ const Contact = () => {
       <div id="contact">
         <h1 className="contactPageTitle">Contact Me</h1>
         <span className="contactDesc">
-          Please fill out the form below to discuss any work opportunities</span>
-          <form className="contactForm">
-            <input  type="text" className="name" placeholder="Your Name"/>
-            <input  type="email" className="name" placeholder="Your Email"/>
-            <textarea className="msg" name="message" rows='5' placeholder="Your Message"></textarea>
-            <button type="submit" value='Send' className="submitBtn">Submit</button>
-            <div className="links">
-                <img src={LinkedIn} className="link" alt="LinkedIn"/>
-                <img src={Github} className="link" alt="Github"/>
-                <img src={X} className="link" alt="X"/>
-                <img src={Insta} className="link" alt="Insta"/>
-            </div>
-          </form>
+          Please fill out the form below to discuss any work opportunities
+        </span>
+        <form className="contactForm">
+          <input type="text" className="name" placeholder="Your Name" />
+          <input type="email" className="name" placeholder="Your Email" />
+          <textarea
+            className="msg"
+            name="message"
+            rows="5"
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" value="Send" className="submitBtn">
+            Submit
+          </button>
+          <div className="links">
+            <a href="https://www.linkedin.com/in/mica-barasa">
+              <img src={LinkedIn} className="link" alt="LinkedIn" />
+            </a>
+            <a href="https://github.com/Barasa-Micah">
+              <img src={Github} className="link" alt="Github" />
+            </a>
+            <a href="https://x.com/BarasaMicah5?t=2cfhuXIWPkRDuet8iS0EEg&s=09">
+              <img src={X} className="link" alt="X" />
+            </a>
+            <a href="https://www.instagram.com/mica.codes?igsh=NXVmdzVkbzNkamJ6">
+              <img src={Insta} className="link" alt="Insta" />
+            </a>
+          </div>
+        </form>
       </div>
     </section>
   );
 };
 
-export default Contact;
+export default Contact;
