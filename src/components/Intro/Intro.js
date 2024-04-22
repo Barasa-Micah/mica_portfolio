ad3a8a23bdf1d1180c41f3035b504b001933061d
username <barasamica@gmai.com>
2024-04-22 23:08:01 +0300

    Added resume

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './Intro.css';
 import bg from '../../assets/profile2.jpg';
-import { Link } from 'react-scroll';
+import resume from '../../assets/Micah-Barasa.pdf'
+
 const Intro = () => {
+  const handleDownloadResume = () => {
+    const resumePDF = require('../../assets/Micah-Barasa.pdf');
+    const link = document.createElement('a');
+    link.href = resumePDF;
+    link.download = {resume}; 
+    link.click();
+  };
+
   return (
     <section id='intro'> 
-    <div className='introContent'>
+      <div className='introContent'>
         <span className='hello'>Hello,</span>
         <span className='introtext'>I'm <span className='introName'>Barasa</span> <br /> A Software Developer</span>
         <p className='introPara'>Explore a world where code creates creativity and <br/> dreams are just a line of code away, with me.</p>
-        <Link><button className='btn'>Resume</button></Link>
-    </div>
-    <img src={bg} alt='profile' className='bg' />
-</section>
-
+        <button className='btn' onClick={handleDownloadResume}>View Resume</button>
+      </div>
+      <img src={bg} alt='profile' className='bg' />
+    </section>
   );
 }
 
